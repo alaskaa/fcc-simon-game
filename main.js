717cec637be54a5bc2cb746a9a3d3b92d0fc947d
Sibylle Sehl <sibylle.sehl@googlemail.com>
2018-02-16 16:03:16 +0000

    added sounds to button presses

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 document.getElementById("arc-one").onclick = () => {
   var col = document.getElementById("arc-one");
   col.classList.add("arc-one-light");
+  let audio = document.getElementById("sound_one");
+  //if(!audio) return;
+  audio.play();
   setTimeout(function() {
     col.classList.remove("arc-one-light"); }, 1000);
 }
@@ -8,6 +11,10 @@ document.getElementById("arc-one").onclick = () => {
 document.getElementById("arc-two").onclick = () => {
   var col = document.getElementById("arc-two");
   col.classList.add("arc-two-light");
+  let audio = document.getElementById("sound_two");
+  //if(!audio) return;
+  //audio.currentTime = 0;
+  audio.play();
   setTimeout(function() {
     col.classList.remove("arc-two-light"); }, 1000);
 }
@@ -15,6 +22,9 @@ document.getElementById("arc-two").onclick = () => {
 document.getElementById("arc-three").onclick = () => {
   var col = document.getElementById("arc-three");
   col.classList.add("arc-three-light");
+  let audio = document.getElementById("sound_three");
+  //if(!audio) return;
+  audio.play();
   setTimeout(function() {
     col.classList.remove("arc-three-light"); }, 1000);
 }
@@ -22,6 +32,9 @@ document.getElementById("arc-three").onclick = () => {
 document.getElementById("arc-four").onclick = () => {
   var col = document.getElementById("arc-four");
   col.classList.add("arc-four-light");
+  let audio = document.getElementById("sound_four");
+  //if(!audio) return;
+  audio.play();
   setTimeout(function() {
     col.classList.remove("arc-four-light"); }, 1000);
 }
